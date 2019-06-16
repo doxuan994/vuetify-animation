1a985d7b7482a4878cb683650e0a93affc8039e1
Xuan Do <doxuancntp@gmail.com>
2019-06-16 10:54:30 -0400

    Commit.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,142 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 md4>
+        <div class="mx-3 grey lighten-1 pa-5">
+          <div class="loader">
+            <!-- <svg class="circular-loader" viewBox="-4 -4 78 78"><circle class="loader-path" cx="35" cy="35" r="32" fill="white" stroke="#f4511e" stroke-width="9" /></svg> -->
+            <svg viewBox="-3 -3 106 106" style="display: block; width: 100%;" class="circular-loader">
+              <path d="M 50,50 m 0,-47.5 a 47.5,47.5 0 1 1 0,95 a 47.5,47.5 0 1 1 0,-95" stroke="white" stroke-width="9" fill-opacity="0"></path>
+              <path d="M 50,50 m 0,-47.5 a 47.5,47.5 0 1 1 0,95 a 47.5,47.5 0 1 1 0,-95" stroke="#F4511E" stroke-width="9" fill-opacity="0" class = "xpath" style="stroke-dasharray: 300, 300; stroke-dashoffset: 255;"></path>
+            </svg>
+
+            <v-btn fab dark color="deep-orange darken-1" class="spinBtn">
+              <v-icon dark>add</v-icon>
+            </v-btn>
+
+          </div>
+
+
+          <!-- <v-btn fab dark color="deep-orange darken-1">
+            <v-icon dark>add</v-icon>
+          </v-btn>
+          <v-btn fab dark color="blue-grey darken-3">
+            <v-icon dark>done</v-icon>
+          </v-btn> -->
+
+
+        </div>
+      </v-flex>
+      <v-flex xs12 md4>
+        <div class="mx-3 deep-purple darken-3 pa-5"></div>
+      </v-flex>
+      <v-flex xs12 md4>
+        <div class="mx-3 white pa-5"></div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data: () => ({
+
+  })
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+* {
+  box-sizing: border-box;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/* Button inside  */
+.spinBtn {
+  margin: 7px 7px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+
+
+
+
+/* SVG Cirlce */
+.loader {
+  position: relative;
+  margin: 6px 8px;
+  width: 70px;
+  height: 70px;
 }
-a {
-  color: #42b983;
+
+/* Spin */
+.circular-loader {
+  transform: rotate(-45deg);
+  animation: rotate 0.8s linear 1;
+  height: 100%;
+  transform-origin: center center;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: auto;
+
+  width: 70px;
+  height: 70px;
+
+  /* background-color: gold; */
 }
+
+/* Define spin path */
+.loader-path {
+  stroke-dasharray: 200,200;
+  stroke-dashoffset: 0;
+  stroke-linecap: round;
+  /* paint-order: stroke; */
+
+  /* animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite; */
+  animation: dash 0.8s ease-in-out 1 forwards;
+}
+
+
+@keyframes rotate {
+  100% {
+    transform: rotate(315deg);
+  }
+}
+
+
+
+
+
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  /* 100% {
+  stroke-dasharray: 1, 200;
+  stroke-dashoffset: 0;
+  } */
+}
+
+
+
+
+
+
+
+/* @keyframes color {
+0% {
+stroke: #F4511E;
+}
+40% {
+stroke: #F4511E;
+}
+66% {
+stroke: #F4511E;
+}
+80%, 90% {
+stroke: #F4511E;
+}
+} */
 </style>
